f3e397f7002daa957eb262f26b623c1dc5096f8d
Evan <siyeel04@gmail.com>
2024-06-11 22:36:08 +0800

    added blur when menu is shown

--- 11 - sliding sidebar/scripts/sidebar.js
+++ 11 - sliding sidebar/scripts/sidebar.js
@@ -1,7 +1,12 @@
 function menu() {
     const menu_container = document.querySelector('.sidebar-container')
+    const overlay = document.querySelector('.overlay')
 
     if (menu_container.style.width !== '300px')
         menu_container.style.width = '300px'
     else menu_container.style.width = '0px'
+
+    if (overlay.style.display === 'block')
+        overlay.style.display = 'none'
+    else overlay.style.display = 'block'
 }
